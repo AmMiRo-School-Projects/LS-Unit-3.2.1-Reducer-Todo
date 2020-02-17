559f4de5b09e7cd75ba42979e07ca33f5ec69d8a
Amos Rose <amosroseacct@gmail.com>
2020-02-17 13:31:32 -0700

    step 1 complete

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,11 @@
+export const initialState = {
+    item: "",
+    completed: false,
+    id: 
+}
+
+export const todoReducer = (state, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+}
